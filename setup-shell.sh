76c3cd4090878e52dd98870df79510d82dc2569c
Felix Leupold <felix@xiel.de>
2020-05-12 21:40:09 +0200

    cleanup setup
    Signed-off-by: Felix Leupold <felix@xiel.de>

--- setup-shell.sh
+++ setup-shell.sh
@@ -17,10 +17,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# install spaceship-prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-# Symlink spaceship.zsh-theme to your oh-my-zsh custom themes directory:
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
 # Remove outdated versions from the cellar.
 brew cleanup
