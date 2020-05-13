78de4f580af03f03761002ee0f5d796ae6fb2ef8
Felix Leupold <felix.leupold-extern@deutschebahn.com>
2020-05-13 11:18:25 +0200

    cleanup add direnv
    Signed-off-by: Felix Leupold <felix.leupold-extern@deutschebahn.com>

--- brew.sh
+++ brew.sh
@@ -20,20 +20,6 @@ ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed --with-default-names
-# Install Bash 4.
-# brew install bash
-# brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-#if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-#  chsh -s "${BREW_PREFIX}/bin/bash";
-#fi;
-
-# Install `wget` with IRI support.
-# brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
@@ -49,55 +35,12 @@ brew install gmp
 # Add Webdev stuff
 brew install yarn
 
-# Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
 # Install other useful binaries.
-# brew install ack
-# brew install exiv2
 brew install git
 brew install git-lfs
-# brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rlwrap
-# brew install ssh-copy-id
-# brew install tree
-# brew install vbindiff
-# brew install zopfli
+
+# like nvm, but for environment variables via .envrc in dictionaries
+brew install direnv
 
 # Remove outdated versions from the cellar.
 brew cleanup
